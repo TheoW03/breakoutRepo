601aee44ab2da0a050dd8899107a10390bd981b6
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-14 12:04:30 -0400

    final touches
    added gui ebents to you die panel and win panel

--- scripts/ButtonActions.cs
+++ scripts/ButtonActions.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class ButtonActions : MonoBehaviour
 {
     // Start is called before the first frame update
